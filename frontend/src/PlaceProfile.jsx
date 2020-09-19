6b83713e63d2d9b4505e910ecacd66bf94575ca0
Zak DeBrine <zdebrine@gmail.com>
2020-09-19 16:53:00 -0600

    Successfully implemented a basic cowlist refactor -- Implementing delete and patch now

--- frontend/src/PlaceProfile.jsx
+++ frontend/src/PlaceProfile.jsx
@@ -3,7 +3,7 @@ import React from 'react';
 var PlaceProfile = (props) => {
     return (
         <div>
-            <img src={props.placeData.image} />
+            <img width="500" height="321" src={props.placeData.image} />
             <h2>{props.placeData.name}</h2>
             <h6>{props.placeData.description}</h6>
         </div>
